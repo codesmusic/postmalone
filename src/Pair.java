f96dff78a3a0d471cd192e53f5079aaeef66f4e7
resolami <wingfly5555@gmail.com>
2017-12-03 19:23:03 +0700

    Update Pair.java

--- src/Pair.java
+++ src/Pair.java
@@ -2,27 +2,52 @@ package tq.s.uml;
 
 import java.awt.*;
 
+/**
+ * Create a pair of points that describe relationship
+ * @author Le trung Thong
+ */
 public class Pair{
-    Point child;
-    Point parent;
+    Point child; // Child point extends parent
+    Point parent; // Parent point
 
+    /**
+     * Constructor creates point
+     * @param start Start point from child diagram
+     * @param end End point from parent diagram
+     */
     public Pair(Point start, Point end) {
         child = start;
         parent = end;
     }
 
+    /**
+     * Child point getter
+     * @return Child point
+     */
     public Point getChild() {
         return child;
     }
 
+    /**
+     * Child setter
+     * @param Child point
+     */
     public void setChild(Point child) {
         this.child = child;
     }
 
+    /**
+     * Parent point getter
+     * @return Parent point
+     */
     public Point getParent() {
         return parent;
     }
 
+    /**
+     * Parent setter
+     * @param Parent child
+     */
     public void setParent(Point parent) {
         this.parent = parent;
     }
